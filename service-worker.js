73c2d8f0d49064e8fe6afcc9b3a631c6da41d4b6
classroom84x-games <Classroom84X@outlook.com>
2025-05-01 16:44:53 -0400

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,21 @@
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open('roblox-cache').then((cache) => {
+            return cache.addAll([
+                '/',
+                '/index.html',
+                '/roblox.png',
+                '/manifest.json',
+                '/service-worker.js'
+            ]);
+        })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((cachedResponse) => {
+            return cachedResponse || fetch(event.request);
+        })
+    );
+});
